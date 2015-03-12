71c4ac8247b14bbe78eeca2d43016fb553601070
Ztt <djkyzy_111@qq.com>
2015-03-12 08:04:56 +0800

    Initial Commit

---
+++ SoftCard803/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SoftCard803
+//
+//  Created by apple on 15/3/12.
+//  Copyright (c) 2015年 iOSlearning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
